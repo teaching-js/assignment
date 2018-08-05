bcdfbd46d0fe75ed334a4f68be23c1f420976ee4
Alex Hinds <alex.hinds141@gmail.com>
2018-08-05 15:59:40 +1000

    coming to life

--- src/main.js
+++ src/main.js
@@ -6,13 +6,29 @@ import API from './api.js';
 
 const api  = new API();
 
-api.getFeed()
+// we can use this single api request multiple times
+const feed = api.getFeed();
+
+feed
 .then(posts => {
     posts.reduce((parent, post) => {
         const img = createElement('img', null, 
-            { src: 'images/'+post.thumbnail, alt: post.meta.description_text, class: 'post' });
+            { src: 'images/'+post.thumbnail, alt: post.meta.description_text, class: 'post-thumb' });
         parent.appendChild(img);
         return parent;
 
     }, document.getElementById('feed'));
+});
+
+feed
+.then(posts => {
+    posts.reduce((parent, post) => {
+        const section = createElement('section', null, { class: 'post' });
+        section.appendChild(createElement('h2', post.meta.author, { class: 'post-title' }))
+        section.appendChild(createElement('img', null, 
+        { src: 'images/'+post.src, alt: post.meta.description_text, class: 'post-image' }))
+        parent.appendChild(section)
+        return parent;
+    }, document.getElementById('large-feed'))
+    posts.map(console.log);
 });
