03b36655f2f795a174bcf4973394353cda4304e8
Zain Afzal <zain.afz@gmail.com>
2018-10-09 15:46:36 +1100

    got freed docs up

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ post_meta = api.model('post_meta',{
     "author": fields.Integer(),
     "description_text": fields.String(),
     "published": fields.String(),
-    "likes": fields.List(fields.String())
+    "likes": fields.List(fields.Integer())
 })
 
 post_details = api.model('post_details',{
@@ -28,6 +28,9 @@ post_details = api.model('post_details',{
   "meta": fields.Nested(post_meta),
   "thumbnail": fields.String(),
   "src": fields.String()
+})
+post_list = api.model('post_list',{
+    "posts" : fields.List(fields.Nested(post_details))
 })
 
 login_details = api.model('login_details', {
@@ -42,7 +45,11 @@ user_details = api.model('user_details', {
     'name':  fields.String(example='greg'),
     'posts': fields.List(fields.Nested(post_details))
 })
-
+user_update_details = api.model('user_details', {
+    'email': fields.String(example='greg@fred.com'),
+    'name':  fields.String(example='greg'),
+    'password': fields.String(example='1234')
+})
 signup_details = api.model('signup_details', {
   'username': fields.String(required=True, example='xX_greginator_Xx'),
   'password': fields.String(required=True, example='1234'),
@@ -50,7 +57,7 @@ signup_details = api.model('signup_details', {
   'name':  fields.String(required=True, example='greg')
 })
 
-auth_details = api.parser().add_argument('Authorization', location='headers')
+auth_details = api.parser().add_argument('Authorization', help="Your Authorization Token in the form Token <AUTH_TOKEN>",location='headers')
 
 # Globals
 
@@ -94,13 +101,13 @@ class Login(Resource):
 @auth.route('/signup')
 class Signup(Resource):
     @auth.response(200, 'Success')
-    @auth.response(400, 'Missing Username/Password/Email')
+    @auth.response(400, 'Malformed Request')
     @auth.response(405, 'Username Taken')
     @api.expect(signup_details)
     def post(self):
         (un,ps,em,n) = unpack(request.json,"username","password","email","name")
         if db.exists("USER").where(username=un):
-            abort(405)
+            abort(405, 'Username Taken')
         t = gen_token()
         db_r = db.insert("USER").with_values(
             curr_token=t,
@@ -123,7 +130,7 @@ class User(Resource):
     def get(self):
         t = request.headers.get('Authorization',None)
         if not t:
-            abort(405)
+            abort(405,'Invalid Authorization Token')
         t = t.split(" ")[1]
         u = db.select("USER").where(curr_token=t).execute()
         return {
@@ -133,6 +140,41 @@ class User(Resource):
             "email": u[3],
             "posts": None
         }
+    @user.response(405, 'Invalid Authorization Token')
+    @api.response(200, 'Success')
+    @api.response(400, 'Malformed user object')
+    @api.expect(auth_details,user_update_details)
+    def put(self):
+        t = request.headers.get('Authorization',None)
+        if not t :
+            abort(405, 'Invalid Authorization Token')
+        if not request.json:
+            abort(400, 'Malformed request')
+        keys = request.json.keys()
+        valid_keys=["password","name","email"]
+        if not sum([k in valid_keys for k in keys]) == len(keys):
+            abort(400, 'Malformed request')
+        t = t.split(" ")[1]
+        u = db.select("USER").where(curr_token=t).execute()
+        u_id = u[0]
+        db.update("USER").where(id=u_id).set(**request.json)
+
+@user.route('/feed')
+class User(Resource):
+    @user.response(405, 'Invalid Authorization Token')
+    @api.expect(auth_details)
+    @user.response(200, 'Success', post_list)
+    @user.param("p","Number of posts to fetch")
+    def get(self):
+        t = request.headers.get('Authorization',None)
+        n = request.form.get('p',None)
+        if not t:
+            abort(405,'Invalid Authorization Token')
+        if not n:
+            abort(400,'Malformed Request')
+        t = t.split(" ")[1]
+        u = db.select("USER").where(curr_token=t).execute()
+        return {}
 
 
 if __name__ == "__main__":
